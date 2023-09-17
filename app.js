ab59bd77c9d4abb739e0112f28b2e3688bcc5540
hthere21 <114700489+hthere21@users.noreply.github.com>
2023-09-16 23:03:10 -0700

    Fixing some features

--- app.js
+++ app.js
@@ -12,3 +12,14 @@ inputs.forEach((input)=> {
         input.parentNode.classList.remove("focus");
     });
 })
+
+
+//Logo clicking
+document.getElementById("portfolioLabel").addEventListener("click", function() {
+    // Navigate to the "introduction.html" page
+    window.location.href = "introduction.html";})
+
+    //Button
+document.querySelector('.close-btn').addEventListener('click', function () {
+  document.getElementById('check').checked = false;
+});
