378c2bdbafd905d8d25780375fa71d62d9ea0c1a
hthere21 <114700489+hthere21@users.noreply.github.com>
2023-09-21 23:23:56 -0700

    Adding scroll-spy

--- app.js
+++ app.js
@@ -19,12 +19,6 @@ inputs.forEach((input)=> {
 //     // Navigate to the "introduction.html" page
 //     window.location.href = "introduction.html";})
 
-//     //Button
-// document.querySelector('.close-btn').addEventListener('click', function () {
-//   document.getElementById('check').checked = false;
-// });
-
-
 
 
 //Scroll to reveal
@@ -61,10 +55,77 @@ const observer = new IntersectionObserver((entries) => {
 
 
 
-  //Popup
-  function toggle() {
+// Popup
+function toggle() {
     var blur = document.getElementById('blur');
-    blur.classList.toggle('active')
     var popup = document.getElementById('popup');
-    popup.classList.toggle('active')
+  
+    if (popup.classList.contains('active')) {
+      // If popup is active, remove 'active' class and overflow: hidden
+      popup.classList.remove('active');
+      blur.classList.remove('active');
+      document.body.style.overflow = ''; // Remove overflow: hidden
+    } else {
+      // If popup is not active, add 'active' class and overflow: hidden
+      popup.classList.add('active');
+      blur.classList.add('active');
+      document.body.style.overflow = 'hidden'; // Add overflow: hidden
+    }
   }
+  
+
+
+
+// Get all sections to spy on
+const sections = document.querySelectorAll("section");
+
+// Get the scroll spy indicator dots
+const dots = document.querySelectorAll(".dot");
+
+// Initialize a variable to keep track of the active section
+let activeSection = null;
+
+// Function to update the scroll spy indicator
+function updateScrollSpy() {
+    console.log(sections)
+    sections.forEach((section, index) => {
+
+        const rect = section.getBoundingClientRect();
+        if (rect.top <= 100 && rect.bottom >= 100) {
+            // Only update if the active section has changed
+            if (activeSection !== section) {
+                // Remove "active" class from all dots
+                dots.forEach((dot) => {
+                    dot.classList.remove("active");
+                });
+
+                // Add "active" class to the corresponding dot
+                dots[index].classList.add("active");
+
+                // Update the active section
+                activeSection = section;
+
+                // Log the section information
+                console.log("Currently at section:", section.id);
+            }
+        }
+    });
+}
+
+
+// Attach a scroll event listener
+window.addEventListener("scroll", updateScrollSpy);
+
+// Initial call to set the initial state
+updateScrollSpy();
+
+// Add click event listeners to dots for scrolling
+dots.forEach((dot) => {
+    dot.addEventListener("click", () => {
+        const sectionId = dot.getAttribute("data-section");
+        const section = document.getElementById(sectionId);
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
+    });
+});
